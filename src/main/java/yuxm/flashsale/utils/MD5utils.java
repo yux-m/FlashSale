c7f70acd83d465f5603773c59475b2d5f0663dc5
yux-m <yuxmeng@seas.upenn.edu>
2022-12-31 16:45:52 -0500

    Password encryption method done.

---
+++ src/main/java/yuxm/flashsale/utils/MD5utils.java
@@ -0,0 +1,59 @@
+package yuxm.flashsale.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * Password encryption utils class.
+ */
+@Component
+public class MD5utils {
+
+    private static final String SALT = "4eilo8trk5";
+
+    /**
+     * Double-layer encryption with md5 hashing.
+     * The only method in this class to be called from other classes.
+     *
+     * @param input user input (password)
+     * @param salt  random salt generated
+     * @return encoded(hashed) password
+     */
+    public static String encrypt(String input, String salt) {
+        return secondLayer(firstLayer(input), salt);
+    }
+
+    /**
+     * MD5 hash with hex encoding.
+     *
+     * @param text original text
+     * @return hashed text
+     */
+    private static String md5(String text) {
+        return DigestUtils.md5Hex(text);
+    }
+
+    /**
+     * Transfer user input to hashed password received by backend.
+     *
+     * @param input input
+     * @return encoded password
+     */
+    private static String firstLayer(String input) {
+        String salted = "" + SALT.charAt(3) + SALT.charAt(9) + input + SALT.charAt(7) + SALT.charAt(4);
+        return md5(salted);
+    }
+
+    /**
+     * Transfer password received by backend to encoded password stored in database.
+     *
+     * @param received   password received at backend
+     * @param randomSalt random salt generated
+     * @return encoded password
+     */
+    private static String secondLayer(String received, String randomSalt) {
+        String salted = "" + randomSalt.charAt(3) + randomSalt.charAt(9) + received + randomSalt.charAt(7) + randomSalt.charAt(4);
+        return md5(salted);
+    }
+
+}
---
+++ src/test/java/yuxm/flashsale/utilsTest/MD5utilsTest.java
@@ -0,0 +1,28 @@
+package yuxm.flashsale.utilsTest;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.jupiter.api.Test;
+import yuxm.flashsale.utils.MD5utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MD5utilsTest {
+    @Test
+    void encryptTest() {
+        //original password
+        String cleartext = "123456";
+        String randomSalt = "randomsalt";
+        //first layer
+        String encrypted = "l5" + cleartext + "ro";
+        System.out.println("manual 1st layer: " + encrypted);
+        encrypted = DigestUtils.md5Hex(encrypted);
+        System.out.println("                  " + encrypted);
+        //second layer
+        encrypted = "dt" + encrypted + "ao";
+        System.out.println("manual 2nd layer: " + encrypted);
+        encrypted = DigestUtils.md5Hex(encrypted);
+        System.out.println("                  " + encrypted);
+        //check encrypted output
+        assertEquals(encrypted, MD5utils.encrypt(cleartext, randomSalt));
+    }
+}
